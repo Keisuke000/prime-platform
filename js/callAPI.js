81c890b965fe03562541d49339d6fdc0bf11337d
Taro <77318899+Ryutaro1997@users.noreply.github.com>
2023-04-19 08:25:00 +0900

    Add files via upload

---
+++ js/callAPI.js
@@ -0,0 +1,29 @@
+var callAPI = (mailAddress, fName, fNameKana, lName, lNameKana, birthDate)=>{
+  // instantiate a headers object
+  var myHeaders = new Headers();
+  // add content type header to object
+  myHeaders.append("Content-Type", "application/json");
+  // using built in JSON utility package turn object to string and store in a variable
+
+  var raw = JSON.stringify({
+    "mailAddress": mailAddress, 
+    "fName":fName, 
+    "fNameKana":fNameKana, 
+    "lName":lName,
+    "lNameKana":lNameKana,
+    "birthDate":birthDate});
+
+  // create a JSON object with parameters for API call and store in a variable
+  var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+  };
+  // make API call with parameters and use promises to get response
+  fetch("YOUR-API-INVOKE-URL", requestOptions)
+  .then(response => response.text())
+  .then(result => alert(JSON.parse(result).body))
+  .catch(error => console.log('error', error));
+}
+
