0441bd5d2f7d87cc810fed617d61515e8177b186
keisuke <ramitsu.k11@gmail.com>
2024-03-20 14:05:41 +0900

    ClientIDの修正

--- js/auth.js
+++ js/auth.js
@@ -13,7 +13,7 @@ function OnCognitoAuthenticateUser() {
 	);
 	var poolData = {
 		UserPoolId: 'ap-northeast-1_DAp8YmJLB', // Your user pool id here
-		ClientId: '7798ncmmova3vp8s34me3v07qnh', // Your client id here
+		ClientId: '6ojrrug3p4qdoqsllefll80bv9', // Your client id here
 	};
 	var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 	var userData = {
